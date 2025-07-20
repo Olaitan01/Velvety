59df0472cb4fb47d90bf9e914235339e0fd6f5c5
unknown <abiodunfatunbi4@gmail.com>
2025-07-20 13:08:30 +0100

    fixed border

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -4,12 +4,12 @@ import Stack from "@mui/material/Stack";
 function ProductCard({ img, price, rating, name }) {
   return (
     <div>
-      <div className="flex flex-row justify-center items-center gap-6 border-t-[0.15em] border-b-[0.15em] border-[#e4ece0] px-[2em]">
+      <div className="flex flex-row justify-center items-center gap-6  px-[2em]">
         <div
           className={`flex flex-col p-[2em] gap-[1.8em] ${
             name === "HOLOCENA"
               ? " border-none"
-              : "border-r-[0.15em] border-[#e4ece0]"
+              : "border-r-[0.15em] border-r-[#e4ece0]"
           }`}
         >
           <div
